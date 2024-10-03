e691672f807a6ce90895dcb586323dc58ece021f
Honey-Joe <honeyjoe943@gmail.com>
2024-10-03 15:34:41 +0530

    dfg

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const { PrismaClient } = require("@prisma/client");
 const express = require("express");
-
+const cors = require('cors');
 const app = express();
 
 app.use(express.json())
 
 const prisma = new PrismaClient
 
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    headers: ['Content-Type', 'Authorization']
+  })); 
 app.get("/test",(req,res)=>{
     res.json({message:"Test"})
 })
