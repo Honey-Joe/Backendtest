d12a61a90f4826a828a5faba5c17f1ef0373258f
Honey-Joe <honeyjoe943@gmail.com>
2024-10-08 14:25:00 +0530

    hello mongo

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const mongoose = require("mongoose")
+
+const app = express();
+
+mongoose.connect("mongodb+srv://honeyjoe942:PPpbUYKPI1erBhhH@techx.gkypa.mongodb.net/Techx")
+
+const userSchema = new mongoose.Schema({
+    name:String,
+    age:Number
+})
+
+const User = mongoose.model('User', userSchema);
+
+
+
+app.post('/users', async (req, res) => {
+    const newUser = new User(req.body);
+    try {
+        const savedUser = await newUser.save();
+        res.json(savedUser);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+})
+
+app.get('/users', async (req, res) => {
+    try {
+        const users = await User.find();
+        res.json(users);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+
+app.get("/", (req,res)=>{
+
+    res.send("Hello")
+
+})
+
+
+
+app.listen(3000)
