032d7b82f304e38aeec3ecbe5981b31a8c6c5cd5
Honey Joe <honeyjoe942@gmail.com>
2024-11-14 13:24:14 +0530

    asd

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ app.post("/email",async (req,res)=>{
         if(user){
             return res.status(400).json({message:"Email already exist"})
         }
-        res.status(200).json({message:"Email available"})
+        res.status(200).json({message:"Email Available"})
 
     }catch(e){
         res.status(500).json({message:e})
